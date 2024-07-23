d2facf6861f11cbcea02cfc6bb22fa52217c67f7
Alex Whiteside <alex.whiteside1@gmail.com>
2024-07-23 19:44:45 -0400

    Update License

--- .projenrc.ts
+++ .projenrc.ts
@@ -4,9 +4,11 @@ const project = new monorepo.MonorepoTsProject({
   devDeps: ["@aws/pdk"],
   name: "clever-ai",
   packageManager: javascript.NodePackageManager.PNPM,
-  pnpmVersion:'9',
+  pnpmVersion:'>=9',
   projenrcTs: true,
-  gitignore:['.idea/']
+  gitignore:['.idea/'],
+  licenseOptions:{disableDefaultLicenses:true},
+  licensed:false
 });
 
 project.package.addEngine('pnpm', '>9')
